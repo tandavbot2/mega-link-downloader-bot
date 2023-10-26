f07446b580c71fd2f637222ed46aef16cdf27cfa
Mr STRATEGIST <142794511+tandavbot2@users.noreply.github.com>
2023-10-26 09:04:20 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,13 +1,13 @@
-import ast
-import redis
-import os
+#import ast
+#import redis
+#import os
 
 
 
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
+#if bool(os.environ.get("WEBHOOK", False)):
+#    from sample_config import Config
+#else:
+  #  from config import Config
 
 #INFO = Config.REDIS_URI.split(":")
 
@@ -29,19 +29,58 @@ else:
 
 
 # Split the URL at "://"
-url_parts = Config.REDIS_URI.split("://")
+#url_parts = Config.REDIS_URI.split("://")
 
 # Split the second part (after "://") using ":"
-host_port_parts = url_parts[1].split(":")
+#host_port_parts = url_parts[1].split(":")
 
 # Extract host and port
-host = host_port_parts[0]
-port = int(host_port_parts[1])
+#host = host_port_parts[0]
+#port = int(host_port_parts[1])
+
+#DB = redis.StrictRedis(
+ #   host=host,
+#    port=port,
+ #   password=Config.REDIS_PASS,
+#    charset="utf-8",
+#    decode_responses=True,
+#)
+
+#def get_stuff(WHAT):
+#    n = []
+ #   cha = DB.get(WHAT)
+ #   if not cha:
+  #      cha = "{}"
+   # n.append(ast.literal_eval(cha))
+   # return n[0]
+
+
+import ast
+import redis
+import os
+
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# Split the URL at "://"
+url_parts = Config.REDIS_URI.split("://")
+
+# Split the second part (after "://") using "@"
+host_port_password_parts = url_parts[1].split("@")
+host_and_port = host_port_password_parts[1]
+
+# Split the host and port
+host, port = host_and_port.split(":")
+
+# Extract the password
+password = host_port_password_parts[0]
 
 DB = redis.StrictRedis(
     host=host,
-    port=port,
-    password=Config.REDIS_PASS,
+    port=int(port),
+    password=password,
     charset="utf-8",
     decode_responses=True,
 )
